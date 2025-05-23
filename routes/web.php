279f1ec47ee2bae00583f454998d6ca611424b04
Seyi Ayo <oluwaseyiayoola97@gmail.com>
2025-05-23 01:29:33 +0100

    Popular movie api consumed for homepage

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoviesController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
 
-Route::get('/movie', function () {
-    return view('show_movie');
-});
+Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show_movie');
